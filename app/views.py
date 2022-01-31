51a71448d7d0ea13a50a4b4a2adaebeac8bce0e9
Artem-N <novitskijartemiy@gmail.com>
2022-01-31 08:22:07 +0200

    add command menu

--- app/views.py
+++ app/views.py
@@ -75,24 +75,6 @@ def help_(update: Update, context: CallbackContext):
                                                                     ' відповідних кнопок з чату.')
 
 
-# def choose_button(update: Update):
-#     keyboard = [
-#         [InlineKeyboardButton('Глянути що у світі відбувається!', callback_data='1')],
-#         [InlineKeyboardButton('Про що гудуть соціальні мережі!', callback_data='2')],
-#         [InlineKeyboardButton('По всякі прикольчики', callback_data='3')],
-#
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     update.message.reply_text('Отже, по що ти до мене прийшов? ', reply_markup=reply_markup)
-#
-#
-# def button(update: Update):
-#     query = update.callback_query
-#     variant = query.data
-#     query.answer()
-#     query.edit_message_text(text=f'Ти обрав: {variant}')
-
-
 @flask_app.route('/init-bot', methods=['GET', 'POST'])
 def init_bot():
     updater = Updater(token=Config.TOKEN_BOT, use_context=True)
@@ -100,13 +82,14 @@ def init_bot():
 
     start_cmd = CommandHandler('start', start)
     help_cmd = CommandHandler('help', help_)
+    menu_cmd = CommandHandler('menu', start)
 
     dispatcher.add_handler(start_cmd)
     dispatcher.add_handler(help_cmd)
+    dispatcher.add_handler(menu_cmd)
     dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
     dispatcher.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
     dispatcher.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
-    # dispatcher.add_handler(CallbackQueryHandler(button))
 
     updater.start_polling()
     return 'Bot started'
